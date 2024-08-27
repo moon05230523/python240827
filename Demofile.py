21e39213036a48a2c8a0fe27ea7d9baad638a0ce
moon <moon05230523@gmail.com>
2024-08-27 15:36:34 +0900

    읽기쓰기

---
+++ Demofile.py
@@ -0,0 +1,11 @@
+#DemoFile.py
+#읽기
+f = open("c:\\work\\text.txt","wt", encoding="utf-8")
+f.write("첫번째라인\n두번째라인\n세번째라인\n")
+f.close()
+
+#읽기(raw string notation)
+f = open(r"c:\work\test.txt","rt",encoding="utf-8")
+result = f.read()
+print(result)
+f.close()
